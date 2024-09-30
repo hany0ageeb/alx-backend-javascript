7a32f76d20a4a4f75fcfb28c936291a0b41250c3
hany0ageeb <hany0ageeb@gmail.com>
2024-09-30 21:08:08 +0300

    Task 10. For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,6 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (const [idx, value] of array.entries()) {
+    array[idx] = appendString + value; // eslint-disable-line no-param-reassign
+  }
+  return array;
+}
