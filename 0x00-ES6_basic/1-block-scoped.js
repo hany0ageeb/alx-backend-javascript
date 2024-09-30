d1557cb21e696dd7b996dc2c4f916120639b86a0
hany0ageeb <hany0ageeb@gmail.com>
2024-09-30 19:45:45 +0300

    Task 1. Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
-    [task, task2] = [task2, task];
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
