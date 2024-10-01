f05b05cf64cc23e429e1c0afe607b30a2d5079b4
hany0ageeb <hany0ageeb@gmail.com>
2024-10-01 19:52:31 +0300

    Fixing file 2-then.js issues with eslint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,12 @@
-export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+export default async function handleResponseFromAPI(promise) {
+  try {
+    await promise;
     console.log('Got a response from the API');
     return {
       status: 200,
       body: 'Success',
     };
-  }).catch(() => {
-    console.log('Got a response from the API');
+  } catch (e) {
     return new Error();
-  });
+  }
 }
