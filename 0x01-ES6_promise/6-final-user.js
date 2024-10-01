b9f2b03f958d5bdc34084f292f15b66b054a0a2a
hany0ageeb <hany0ageeb@gmail.com>
2024-10-01 20:20:36 +0300

    Fixing file 6-final-user.js issues

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const result = [];
+  try {
+    let value = await signUpUser(firstName, lastName);
+    result.push({ status: 'Fulfilled', value });
+    value = await uploadPhoto(fileName);
+    result.push({ status: 'Fulfilled', value });
+  } catch (e) {
+    result.push({ status: 'Rejected', value: e });
+  }
+  return new Promise((() => result));
 }
