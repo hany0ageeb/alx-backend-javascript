77a622b89c448b2cbd3475f9be412160b2ed8756
hany0ageeb <hany0ageeb@gmail.com>
2024-10-03 11:14:39 +0200

    fixing 6-finsl-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,11 +5,11 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   const result = [];
   try {
     let value = await signUpUser(firstName, lastName);
-    result.push({ status: 'Fulfilled', value });
+    result.push({ status: 'fulfilled', value });
     value = await uploadPhoto(fileName);
-    result.push({ status: 'Fulfilled', value });
+    result.push({ status: 'fulfilled', value });
   } catch (e) {
-    result.push({ status: 'Rejected', value: e });
+    result.push({ status: 'rejected', value: e.toString() });
   }
-  return new Promise((() => result));
+  return Promise.resolve(result);
 }
