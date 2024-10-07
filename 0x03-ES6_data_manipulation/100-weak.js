fcc8de0767f4ec532a2571c387c93e13368addfd
hany0ageeb <hany0ageeb@gmail.com>
2024-10-07 12:21:30 +0200

    Task 11. Weak link data structure - Project 0x03-es6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,9 @@
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    if (weakMap.get(endpoint) < 5) weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+    else throw new Error('Endpoint load is high');
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}
