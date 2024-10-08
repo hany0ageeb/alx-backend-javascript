549e0aa97bb97d42d5b4a803554f7b5354a7df48
hany0ageeb <hany0ageeb@gmail.com>
2024-10-08 13:18:09 +0200

    change clean_set function to use array.from

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || startString === '' || set.size === 0) return '';
+  const str = String(startString);
+  if (str.length === 0 || str === '' || set.size === 0) return '';
   function isValid(value) {
     if (value.startsWith(startString)) return true;
     return false;
   }
-  return Array.from(set).filter((value) => isValid(value)).map((value) => value.substring(startString.length)).join('-');
+  return Array.from(set).filter((value) => isValid(value)).map((value) => value.substring(str.length)).join('-');
 }
