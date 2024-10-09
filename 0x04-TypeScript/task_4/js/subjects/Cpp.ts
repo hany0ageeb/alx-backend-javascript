de1d3ea6db034c83801c86c96fd7a54578e8a72e
hany0ageeb <hany0ageeb@gmail.com>
2024-10-09 12:44:21 +0200

    Task 4

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,15 @@
+namespace Subjects{
+    export interface Teacher{
+        experienceTeachingC: number;
+    }
+    class Cpp extends Subjects.Subject{
+        getRequirements(): string{
+            return 'Here is the list of requirements for Cpp';
+        }
+        getAvailableTeacher(): string{
+            if(this.teacher.experienceTeachingC > 0)
+                return `Available Teacher: ${this.teacher.firstName}`;
+            return 'No available teacher';
+        }
+    }
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,8 @@
+namespace Subjects {
+    export class Subject{
+        teacher: Teacher;
+        setTeacher(teacher: Subjects.Teacher): void{
+            this.teacher = teacher;
+        }
+    }
+}
