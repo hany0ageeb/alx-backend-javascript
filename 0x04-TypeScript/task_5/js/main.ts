ecf270cb20e5b7189e0edfef1b9a904a7f9eca4a
hany0ageeb <hany0ageeb@gmail.com>
2024-10-09 13:56:02 +0200

    Task 5

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -7,18 +7,18 @@ interface MinorCredits{
     readonly brand: 'MinorCredits' = 'MinorCredits';
 }
 function sumMajorCredits(subject1: MajorCredits | MinorCredits, subject2: MajorCredits | MinorCredits): MajorCredits{
-    return [subject1, subject2].filter(subj => subj.brand === 'MajorCredits').reduce((pre, cur) =>{
+    return [subject1, subject2].filter(subj => subj.brand === 'MajorCredits').map(subj => subj as MajorCredits).reduce((pre, cur) =>{
         return {
             credits: pre.credits + cur.credits,
             brand: 'MajorCredits',
         };
-    }, {credits: 0});
+    }, {credits: 0, brand: 'MajorCredits'});
 }
 function sumMinorCredits(subject1: MajorCredits | MinorCredits, subject2: MajorCredits | MinorCredits): MinorCredits{
-    return [subject1, subject2].filter(subj => subj.brand === 'MinorCredits').reduce((pre, cur) =>{
+    return [subject1, subject2].filter(subj => subj.brand === 'MinorCredits').map(subj => subj as MajorCredits).reduce((pre, cur) =>{
         return {
             credits: pre.credits + cur.credits,
             brand: 'MinorCredits',
         };
-    }, {credits: 0});
+    }, {credits: 0, brand: 'MinorCredits'});
 }
