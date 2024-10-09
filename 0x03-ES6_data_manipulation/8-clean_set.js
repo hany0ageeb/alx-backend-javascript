10b910416555e857b6f48b957a377df6582b27c1
hany0ageeb <hany0ageeb@gmail.com>
2024-10-09 08:28:44 +0200

    Task 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,5 @@
 export default function cleanSet(set, startString) {
   const str = String(startString);
   if (str.length === 0 || str === '' || set.size === 0) return '';
-  function isValid(value) {
-    if (value.startsWith(startString)) return true;
-    return false;
-  }
-  return Array.from(set).filter((value) => isValid(value)).map((value) => value.substring(str.length)).join('-');
+  return [...set].filter((value) => typeof value === 'string' && value.startsWith(str)).map((value) => value.substring(str.length)).join('-');
 }
