627c14919703250995deb8d48b303e23aa75856e
hany0ageeb <hany0ageeb@gmail.com>
2024-12-02 12:48:09 +0200

    changing tests

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,7 +1,7 @@
 const chai = require("chai");
 const calculateNumber = require("./1-calcul");
 
-describe('testing calculateNumber', function(){
+describe('testing calculateNumber with SUM as type', function(){
     it('calculateNumber(SUM, 1, 3) should return 4', function(){
         chai.expect(calculateNumber('SUM', 1, 3)).to.equal(4);
     });
@@ -29,7 +29,9 @@ describe('testing calculateNumber', function(){
     it('calculateNumber(SUM, 1.2, 3.3) should return 4', function(){
         chai.expect(calculateNumber('SUM', 1.2, 3.3)).to.equal(4);
     });
-    // 
+});
+
+describe('testing calculateNumber with SUBTRUCT as type', function(){
     it('calculateNumber(SUBTRACT, 1, 3) should return -2', function(){
         chai.expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
     });
@@ -56,7 +58,9 @@ describe('testing calculateNumber', function(){
     it('calculateNumber(SUBTRACT, 1.2, 3.3) should return -2', function(){
         chai.expect(calculateNumber('SUBTRACT', 1.2, 3.3)).to.equal(-2);
     });
-    //
+});
+
+describe('testing calculateNumber with DIVIDE as type', function(){
     it('calculateNumber(DIVIDE, 1, 0) should return Error', function(){
         chai.expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error');
     });
@@ -69,11 +73,9 @@ describe('testing calculateNumber', function(){
     it('calculateNumber(DIVIDE, 4, 2) should return 2', function(){
         chai.expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
     });
-
     it('calculateNumber(DIVIDE, 1.5, 2) should return 1', function(){
         chai.expect(calculateNumber('DIVIDE', 1.5, 2)).to.equal(1);
     });
-
     it('calculateNumber(DIVIDE, 1.2, 2) should return 0.5', function(){
         chai.expect(calculateNumber('DIVIDE', 1.2, 2)).to.equal(0.5);
     });
@@ -85,7 +87,6 @@ describe('testing calculateNumber', function(){
     it('calculateNumber(DIVIDE, 2, 3.6) should return 0.5', function(){
         chai.expect(calculateNumber('DIVIDE', 2, 3.6)).to.equal(0.5);
     });
-
     it('calculateNumber(DIVIDE, 1.2, 3.6) should return 0.25', function(){
         chai.expect(calculateNumber('DIVIDE', 1.2, 3.6)).to.equal(0.25);
     });
