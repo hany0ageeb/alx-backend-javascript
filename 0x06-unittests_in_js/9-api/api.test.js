9daca636be77684675ca99f41819bb7918077928
hany0ageeb <hany0ageeb@gmail.com>
2024-12-02 18:47:39 +0200

    task 9. Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -26,7 +26,16 @@ describe('Integration Test', () => {
             });
         });
         it('status code 404', (done) => {
-            request.get('http://localhost:7865/car/a', (err, res, body) => {
+            request.get('http://localhost:7865/car/hello', (err, res, body) => {
+                if(err){
+                    done(err);
+                }
+                expect(res.statusCode).to.equal(404);
+                done();
+            });
+        });
+        it('status code 404', (done) => {
+            request.get('http://localhost:7865/cart/1a', (err, res, body) => {
                 if(err){
                     done(err);
                 }
